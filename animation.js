e5ca9e3cee25d5c7d7ee27b16d36b770d1112100
rata sorin <ratasorin0@gmail.com>
2021-02-09 11:30:43 +0200

    asd

---
+++ animation.js
@@ -0,0 +1,80 @@
+const STAY_DOWN = (function () {
+  //metode private
+  let state;
+  let controller;
+  let engine;
+  let loader;
+  let renderer;
+  return {
+    constructors: {},
+    image: {
+      alessia: undefined,
+      frameSets: [
+        [0, 1, 2, 3, 4, 5, 6, 7],
+        [8, 9, 10, 11, 12, 13, 14, 15],
+        [16, 17, 18, 19],
+        [20, 21, 22, 23],
+      ],
+    },
+    managers: {},
+    states: {},
+
+    display: document.createElement("canvas").getContext("2d", { alpha: true }),
+
+    //changeState
+    changeState(currentState) {
+      currentState.deactivate();
+      state = currentState;
+      currentState.activate();
+      engine.setState(currentState);
+    },
+    //getter
+    getEngine() {
+      return engine;
+    },
+    getController() {
+      return controller;
+    },
+    getLoader() {
+      return loader;
+    },
+    getRenderer() {
+      return renderer;
+    },
+
+    //initializer
+    initialize() {
+      const {
+        image,
+        display,
+
+        states: { run },
+      } = this;
+      loader.loadImages(["alessia.png"], function (images) {
+        console.log(images);
+        image.alessia = images[0];
+        console.log(image.alessia);
+        controller.activate();
+        document.body.appendChild(display.canvas);
+        STAY_DOWN.changeState(run);
+        engine.start();
+      });
+
+      window.addEventListener("resize", run.resize);
+    },
+
+    //setters
+    setController(controllerSetter) {
+      controller = controllerSetter;
+    },
+    setEngine(engineSetter) {
+      engine = engineSetter;
+    },
+    setLoader(loaderSetter) {
+      loader = loaderSetter;
+    },
+    setRenderer(rendererSetter) {
+      renderer = rendererSetter;
+    },
+  };
+})();
---
+++ controller.js
@@ -0,0 +1,67 @@
+STAY_DOWN.setController(
+  (function (window) {
+    //these are private because of the anonymous function
+    const input = function () {
+      this.active = false;
+      this.state = false;
+      this.lastPressed = false;
+    };
+    input.prototype = {
+      trigger(state) {
+        if (state != this.state) this.active = this.state = state;
+      },
+      makeLastPressed() {
+        this.lastPressed = true;
+      },
+      dismissLastPressed() {
+        this.lastPressed = false;
+      },
+    };
+    const left = new input();
+    const right = new input();
+    const up = new input();
+    const p = new input();
+
+    function keyUpDown(e) {
+      e.preventDefault();
+      state = e.type == "keydown";
+      switch (e.keyCode) {
+        case 37:
+          left.trigger(state);
+          break;
+        case 38:
+          up.trigger(state);
+          break;
+        case 39:
+          right.trigger(state);
+          break;
+        case 80:
+          p.trigger(state);
+          break;
+      }
+    }
+
+    // these are public because we return them
+    return {
+      getLeft() {
+        return left.active;
+      },
+      getRight() {
+        return right.active;
+      },
+      getUp() {
+        return up.active;
+      },
+      getP() {
+        return p.active;
+      },
+      setP(active) {
+        p.active = active;
+      },
+      activate() {
+        window.addEventListener("keydown", keyUpDown);
+        window.addEventListener("keyup", keyUpDown);
+      },
+    };
+  })(window)
+);
