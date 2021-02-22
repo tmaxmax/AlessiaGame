42969fe7e0e399030453ce96f8bb7656aa068ffe
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-02-22 10:04:43 +0200

    Move files

--- managers/items.manager.js
+++
@@ -1,9 +0,0 @@
-STAY_DOWN.managers.items_manager = (function () {
-  const Item = STAY_DOWN.constructors.Item;
-  return {
-    items_array: [],
-    addItem(x, y) {
-      this.items_array.push(new Item(x, y, 70, 70));
-    },
-  };
-})();
